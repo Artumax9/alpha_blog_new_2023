10fff06b26e138612100fe2a8b42d72fdb7bc2db
Arturo Paredes <arturojp18@gmail.com>
2023-08-20 20:44:23 -0300

    fix turbo featuring to show the confirmation dialog in the delete button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,10 +15,30 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
       if @article.save
-        redirect_to article_path(@article)   
+        flash[:notice] = "The article was created succesfully"
+        redirect_to article_path(@article)  
       else
         render 'new'        
       end
   end
 
+  def edit 
+    @article = Article.find(params[:id])    
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "The article was updated succesfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
